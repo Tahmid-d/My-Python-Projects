bdc987ee7d6d825b2e93f407073abfea07e0d14b
Tahmid-d <tohminabegum0011@gmail.com>
2026-02-27 00:27:03 +0600

    Add files via upload

---
+++ newfile.py
@@ -0,0 +1,6 @@
+# Eta ekta choto robot programme 
+print (" ....  Robot Conversation  .... ")
+name = input(" Apnar name ki ? ")
+age=input(" Apnar boyosh koto ? ")
+print (" Hello " + name + " ! " )
+print ("Wow, " + age + " bochor boyoshe programming shikhcho! Shera hobe")
